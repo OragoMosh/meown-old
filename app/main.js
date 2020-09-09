567228ba290715613b9a66675250152f3c755267
Glitch (meow-test) <none>
2020-09-09 17:24:16 +0000

    🐧📍 Updated with Glitch

--- app/main.js
+++ app/main.js
@@ -602,9 +602,11 @@ var time = new Date();
   socket.on('get account', function (data) {
     alert(data);
   });
+  
   // Whenever the server emits 'new message', update the chat body
   socket.on('new message', function (data) {
     addChatMessage(data);
+    alert(data)
   });
 
   // Whenever the server emits 'user joined', log it in the chat body
